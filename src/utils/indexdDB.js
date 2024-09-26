086b9d1cde0a90ee6ba1994141ca731469d6f94e
vaishnavi-m24 <vaishnavi.moharekar@tekditechnologies.com>
2024-09-26 14:27:29 +0530

    DB

--- src/utils/indexdDB.js
+++ src/utils/indexdDB.js
@@ -1,6 +1,6 @@
 import { openDB } from 'idb';
 
-export const initDB = async () => {
+export const studentDB = async () => {
   return openDB('student-db', 1, {
     upgrade(db) {
       if (!db.objectStoreNames.contains('students')) {
@@ -11,21 +11,21 @@ export const initDB = async () => {
 };
 
 export const addStudent = async (student) => {
-  const db = await initDB();
+  const db = await studentDB();
   return db.add('students', student);
 };
 
 export const getStudents = async () => {
-  const db = await initDB();
+  const db = await studentDB();
   return db.getAll('students');
 };
 
 export const updateStudent = async (student) => {
-  const db = await initDB();
+  const db = await studentDB();
   return db.put('students', student);
 };
 
 export const deleteStudent = async (id) => {
-  const db = await initDB();
+  const db = await studentDB();
   return db.delete('students', id);
 };
